30cfd45870564c317e1f4188bbfd2e4e07291444
Abhishek Herbert Samuel <86236032+AbhishekHerbertSamuel@users.noreply.github.com>
2024-11-06 15:06:44 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,38 +1,12 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSignup = () => {
-    axios.post('http://localhost:5000/signup', { username, password })
-      .then(response => setMessage(response.data))
-      .catch(error => setMessage(error.response.data));
-  };
-
-  const handleLogin = () => {
-    axios.post('http://localhost:5000/login', { username, password })
-      .then(response => setMessage(response.data))
-      .catch(error => setMessage(error.response.data));
-  };
-
-  return (
-    <div>
-      <h2>Signup</h2>
-      <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-      <button onClick={handleSignup}>Sign Up</button>
-
-      <h2>Login</h2>
-      <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-      <button onClick={handleLogin}>Login</button>
-
-      <p>{message}</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello from the Frontend!</h1>
+        </div>
+    );
 }
 
 export default App;
+
